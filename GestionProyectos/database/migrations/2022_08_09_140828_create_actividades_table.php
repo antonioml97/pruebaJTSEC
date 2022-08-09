ca02198a312f5a51a78ef9b3b0494d3f1a961aab
Antonio Martin <antoniomartinleon97@gmail.com>
2022-08-09 16:35:06 +0200

    Creacion de tablas incidencias, actividades y proyectos

---
+++ GestionProyectos/database/migrations/2022_08_09_140828_create_actividades_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actividades', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->unsignedBigInteger('proyecto_id');
+            $table->string('nombre_actividad');
+            $table->string('incidencias')->nuleable();
+            $table->string('usuarios')->nuleable();
+
+            $table->foreign('proyecto_id')->references('id')->on('proyectos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actividades');
+    }
+};
---
+++ GestionProyectos/database/migrations/2022_08_09_140847_create_incidencias_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incidencias', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+
+            $table->unsignedBigInteger('actividades_id');
+            $table->string('nombre_incidencia');
+            $table->string('usuarios')->nuleable();
+
+
+            $table->foreign('actividades_id')->references('id')->on('proyectos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incidencias');
+    }
+};
